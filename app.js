9465cb1e4a3d8113a979472379b27ae8b0e73524
root <root@ip-172-31-37-20.ec2.internal>
2020-05-02 13:08:14 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const newBookAppToAdmin = require('./newBookAppToAdmin');
+const newBookAppToUser = require('./newBookAppToUser');
+const newValidationAppToUser = require('./newValidationAppToUser');
+const newValidationAppToAdmin = require('./newValidationAppToAdmin');
+const successValidation = require('./successValidation');
+const newContactAppToAdmin = require('./newContactAppToAdmin');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.post('/newBookAppToAdmin', (req, res) => {
+	newBookAppToAdmin(req.body);
+	res.status(200).send;
+})
+app.post('/newBookAppToUser', (req, res) => {
+	newBookAppToUser(req.body);
+	res.status(200).send;
+})
+app.post('/newValidationAppToUser', (req, res) => {
+	newValidationAppToUser(req.body);
+	res.status(200).send;
+})
+app.post('/newValidationAppToAdmin', (req, res) => {
+	newValidationAppToAdmin(req.body);
+	res.status(200).send;
+})
+app.post('/successValidation', (req, res) => {
+	successValidation(req.body);
+	res.status(200).send;
+})
+app.post('/newContactAppToAdmin', (req, res) => {
+	newContactAppToAdmin(req.body);
+	res.status(200).send;
+})
+app.listen(3005, () => {
+console.log('Servidor corriendo')
+});
+
